dfaa77dcb0cb0389865b6d3770849616de810898
Kevin Cheng <dcvsling@gmail.com>
2025-08-21 18:42:08 +0800

    init

---
+++ app/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new app.Game1();
+game.Run();
---
+++ src/Particle/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+using Particle.Abstractions;
+
+namespace Particle;
+
+
+internal ref struct Particles<T>(int capacity) : IDisposable where T : struct 
+{
+    public ref T this[int index] => ref _owner.Memory.Span[index];
+    public int Capacity => capacity;
+    private IMemoryOwner<T> _owner = MemoryPool<T>.Shared.Rent(capacity);
+    public Span<T>.Enumerator GetEnumerator() => _owner.Memory.Span.GetEnumerator();
+
+    public void Dispose()
+        => _owner.Dispose();
+}
